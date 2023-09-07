a0238244e8aea95e8ada4ff4bbef4f803fd3c11b
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-07 10:46:46 +0100

    added test for deposit and withdraw method

--- src/account.test.ts
+++ src/account.test.ts
@@ -1,6 +1,19 @@
 import { Account } from "./account";
-const myAccount = new Account(123, 100); //id: 123, starting balance: 100
 
 test("toString to return the details from the Account class", () => {
+    const myAccount = new Account(123, 100);
+
     expect(myAccount.toString()).toBe("Account id: 123, balance: 100");
+});
+
+test("testing the deposit method in Account class, it should increase the starting balance", () => {
+    const myAccount = new Account(123, 100);
+    myAccount.deposit(20);
+    expect(myAccount.toString()).toBe("Account id: 123, balance: 120");
+});
+
+test("testing the withdraw method in Account class, it should decrease the starting balance", () => {
+    const yourAccount = new Account(42, 999);
+    yourAccount.withdraw(900);
+    expect(yourAccount.toString()).toBe("Account id: 42, balance: 99");
 });
--- src/account.ts
+++ src/account.ts
@@ -1,24 +1,19 @@
-/*const myAccount = new Account(123, 100);//id: 123, starting balance: 100
-const yourAccount = new Account(42, 999); //id: 42, starting balance: 999
-
-myAccount.deposit(20);
-
-yourAccount.withdraw(900);
-
-console.log(myAccount.toString()); //"Account id: 123, balance: 120"
-console.log(yourAccount.toString()); //"Account id: 42, balance 99" */
-
 export class Account {
-    accountId: Number;
-    startBalance: Number;
+    accountId: number;
+    startBalance: number;
     constructor(accountId: number, startBalance: number) {
         this.accountId = accountId;
         this.startBalance = startBalance;
+    }
+
+    deposit(amount: number) {
+        this.startBalance += amount;
+    }
+
+    withdraw(amount: number) {
+        this.startBalance -= amount;
     }
     toString() {
         return `Account id: ${this.accountId}, balance: ${this.startBalance}`;
     }
 }
-
-// const myAccount = new Account(123, 100); //id: 123, starting balance: 100
-// const yourAccount = new Account(42, 999); //id: 42, starting balance: 999
